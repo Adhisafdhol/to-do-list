76c580c8d25b7c3741efb13fb14ea81240476746
Aadheuss <aadheusx@gmail.com>
2023-09-11 02:49:22 +0700

    Add sidebar button

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,11 @@ function home () {
   mainHeader.classList.add('main-header');
   homeContainer.appendChild(mainHeader);
 
+  const sidebarBtn = document.createElement('button');
+  sidebarBtn.classList.add('sidebar-btn');
+  sidebarBtn.setAttribute('type', 'button');
+  mainHeader.appendChild(sidebarBtn)
+
   const mainHeading = document.createElement('h1');
   mainHeading.textContent = 'Task.Daily';
   mainHeader.appendChild(mainHeading);
@@ -13,4 +18,14 @@ function home () {
   return homeContainer;
 }
 
+function sidebar() {
+  const sidebar = document.createElement('nav');
+  sidebar.classList.add('sidebar');
+
+  const allTask = document.createElement('div');
+  allTask.setAttribute('data-key', 'all-task');
+  sidebar.appendChild(allTask);
+
+  return sidebar;
+}
 export { home };
